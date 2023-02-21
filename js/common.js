397023b2f32224a2cc3dbb64393bd7630e11a8ff
김성은 <kse012050@naver.com>
2023-02-21 16:42:31 +0900

    서비스 페이지 완료

--- js/common.js
+++ js/common.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
     $('.mainPage').length && mainPage();
+    $('.servicePage').length && servicePage();
 });
 
 function mainPage(){
     $(window).scroll(function(){
         $(this).scrollTop() > 0 ? $('header').removeClass('active') : $('header').addClass('active');
     })
+}
+
+function servicePage(){
+    var swiper = new Swiper(".serviceSlider", {
+        slidesPerView: "auto",
+        spaceBetween: 60,
+        freeMode: true,
+        scrollbar: {
+            el: ".swiper-scrollbar",
+        },
+    });
 }
