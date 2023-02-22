34ff60b6f682c2c79b00d8244b8e8f120e904ddf
김성은 <kse012050@naver.com>
2023-02-23 01:25:30 +0900

    이용약관 페이지 완료

--- js/common.js
+++ js/common.js
@@ -1,16 +1,26 @@
 $(document).ready(function(){
+    // 메인페이지
     $('.mainPage').length && mainPage();
+
+    // 서비스 페이지
     $('.servicePage').length && servicePage();
-    $('.eventPage').length && eventPage();
+
+    // 게시판 드랍 박스 이벤트
+    $('.boardBox-drop').length && boardDrop()();
+    
 });
 
+// 메인페이지
 function mainPage(){
+    // 해더 이벤트
     $(window).scroll(function(){
         $(this).scrollTop() > 0 ? $('header').removeClass('active') : $('header').addClass('active');
     })
 }
 
+// 서비스 페이지
 function servicePage(){
+    // 슬라이더
     var serviceSlider = new Swiper(".serviceSlider", {
         slidesPerView: "auto",
         spaceBetween: 60,
@@ -21,23 +31,38 @@ function servicePage(){
     });
 }
 
-function eventPage(){
-    $('.eventPage .boardArea li a').click(function(e){
+// 게시판 드랍 박스 이벤트
+function boardDrop(){
+    $('.boardBox-drop li a').click(function(e){
+        boardDropEvent($(this) , e)
+    })
+
+    $('.boardBox-drop li a label').click(function(e){
+        $('.boardBox-drop li a').off('click');
+        setTimeout(()=>{
+            $('.boardBox-drop li a').on('click',function(e){
+                boardDropEvent($(this) , e)
+            })
+        },100)
+    })
+    
+    function boardDropEvent(selector , e){
         e.preventDefault();
-        if($(this).next('.editor').is(':animated')) return;
-        if($(this).hasClass('active')){
-            $(this).removeClass('active');
-            $(this).stop().next().slideUp(function(){
-                $(this).next().remove();
+        if(selector.next().is(':animated')) return;
+        if(selector.hasClass('active')){
+            selector.removeClass('active');
+            selector.stop().next().slideUp(function(){
+                $('.eventPage').length && selector.next().remove();
             });
         }else{
-            $(this).parent().siblings().find('a').removeClass('active');
-            $(this).parent().siblings().find('.editor').slideUp(function(){
-                $(this).next().remove();
+            selector.parent().siblings().find('a').removeClass('active');
+            selector.parent().siblings().find('> div').slideUp(function(){
+                $('.eventPage').length && $(this).remove();
             });
-            $(this).addClass('active');
-            $(this).after('<div class="editor">내용</div>')
-            $(this).stop().next().slideDown();
+            selector.addClass('active');
+            // api 추가
+            $('.eventPage').length && selector.after('<div class="editor">내용</div>')
+            selector.stop().next().slideDown();
         }
-    })
+    }
 }
